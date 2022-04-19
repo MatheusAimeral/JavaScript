58fa5e7b804a8a7cd3af218a2fb0dbdeda87874d
matheus-n-alcantara <matheus.a.n.alcantara@gmail.com>
2022-04-19 03:41:06 -0300

    Atualizando os exercicios do curso de JS, modulo D

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 80
+console.log(`A velocidade do seu carro é ${vel}Km/h` )
+if  (vel > 80) { //Condicão simples!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de seguranca!`)
